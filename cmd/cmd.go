cdcb98465f368c400532b6bc7c63ec7ef8117934
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2023-11-03 00:51:27 +0000

    ✨ enable new entry command

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/ahodieb/papyrus/editor"
 	"github.com/ahodieb/papyrus/notes"
@@ -19,11 +21,24 @@ func Run() {
 		Notes:  notes.ReadFromFile(cfg.File),
 	}
 
-	// Default action:
-	// Open the notes file at the latest position
-	err := m.OpenOrCreateLatest()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	args := os.Args[1:]
+	if len(args) == 0 {
+		// Default action:
+		// Open the notes file at the latest position
+		err := m.OpenOrCreateLatest()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+
+	switch args[0] {
+	default:
+		p := m.AddEntry(strings.Join(args, " "), time.Now())
+		err := m.Open(p)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 }
